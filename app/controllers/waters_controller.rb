bac0963781a582695fe8d88b7dfbc94c1b9aca89
Denis Velic <denis.velic@laposte.net>
2022-12-02 12:29:51 +0100

    Merge water points and race points

--- app/controllers/waters_controller.rb
+++ app/controllers/waters_controller.rb
@@ -8,6 +8,7 @@ class WatersController < ApplicationController
     @water = Water.find(params[:id])
     @gpx_file = @water.gpx_file
     @water_markers = parse_gpx('db/seeds/fixtures/paysloire.txt')
+    @markers = parse_gpx_race('db/seeds/fixtures/race7.txt')
   end
 
   private
@@ -22,5 +23,17 @@ class WatersController < ApplicationController
       [lng, lat]
     end
     amenity
+  end
+
+  def parse_gpx_race(filepath)
+    file = File.open(filepath)
+    doc = Nokogiri::XML(file)
+    trackpoints = doc.xpath('//xmlns:trkpt')
+    route = trackpoints.map do |trkpt|
+      lat = trkpt.xpath('@lat').to_s.to_f
+      lng = trkpt.xpath('@lon').to_s.to_f
+      [lng, lat]
+    end
+    route
   end
 end
--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -16,7 +16,7 @@ export default class extends Controller {
       container: this.element,
       style: "mapbox://styles/mapbox/streets-v12",
       center: this.markersValue[0],
-      zoom: 7
+      zoom: 10
     })
     this.map.on('load', () => {
       this.map.addSource('route', {
