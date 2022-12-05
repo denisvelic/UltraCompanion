185dc66a66cc132cd450c7d25c207dd44de2c309
pierrecaro <pierre.caro@gmail.com>
2022-12-05 15:57:21 +0100

    Seeds updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_05_110957) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_05_133506) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -65,6 +65,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_05_110957) do
     t.text "comp_distance"
     t.text "comp_elevation"
     t.text "comp_time"
+    t.string "status"
     t.index ["user_id"], name: "index_races_on_user_id"
   end
 
