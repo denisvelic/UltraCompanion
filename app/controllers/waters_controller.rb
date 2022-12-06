2d05860e0cdc317663b69350f3499c1e4c46d8ef
Denis Velic <denis.velic@laposte.net>
2022-12-06 13:29:17 +0100

    debug mode

--- app/controllers/waters_controller.rb
+++ app/controllers/waters_controller.rb
@@ -9,7 +9,7 @@ class WatersController < ApplicationController
 
     # @gpx_file = @water.gpx_file
     @race = Race.find(params[:race_id])
-    @water_markers = parse_gpx('db/seeds/fixtures/paysloire.txt')
+    @water_markers = parse_gpx('db/seeds/fixtures/water_west_france.txt')
     @markers = parse_gpx_race(@race.gpx_file)
   end
 
@@ -18,28 +18,26 @@ class WatersController < ApplicationController
   def parse_gpx(filepath)
     file = File.open(filepath)
     doc = Nokogiri::XML(file)
+    trackpoints = doc.xpath('//xmlns:wpt')
+    amenity = trackpoints.map do |wpt|
+      {
+        lat: wpt.xpath('@lat').to_s.to_f,
+        lng: wpt.xpath('@lon').to_s.to_f,
+        image_url: helpers.asset_url("icons/bottle_true.svg")
+      }
+    end
+    amenity
+  end
+
+  def parse_gpx_race(filepath)
+    file = File.open(filepath)
+    doc = Nokogiri::XML(file)
     trackpoints = doc.xpath('//xmlns:trkpt')
-    # funnel = Array.new
     route = trackpoints.map do |trkpt|
       lat = trkpt.xpath('@lat').to_s.to_f
       lng = trkpt.xpath('@lon').to_s.to_f
-      # ele = trkpt.text.strip.to_f
       [lng, lat]
     end
-    # route.map do |coor|
-    #   if route.index(coor) % (route.length/100) == 0
-    #    funnel.push(coor)
-    #   end
-    #  end
-    # funnel
-    route ## Pour test, on renvoie les 4 premiers points
-  end
-
-  def race_params
-    params.require(:race).permit(:name, :date, :started_at)
-  end
-
-  def set_race
-    @race = Race.find(params[:id])
+    route
   end
 end
