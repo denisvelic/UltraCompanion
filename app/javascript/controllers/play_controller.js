a9ad70f80de4a49046976a2d074e60f2dee2db82
Denis Velic <denis.velic@laposte.net>
2022-12-06 17:13:07 +0100

    setup method play pause map live

---
+++ app/javascript/controllers/play_controller.js
@@ -0,0 +1,12 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="play"
+export default class extends Controller {
+  static targets = ["icon"]
+
+  connect() {
+    // console.log("hello");
+  }
+
+
+}
