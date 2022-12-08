d44c5a3bb19ccf581fafb3ac3a1c7a2d6c01df71
Denis Velic <denis.velic@laposte.net>
2022-12-08 15:55:24 +0100

    Play/Pause button working

--- app/javascript/controllers/play_controller.js
+++ app/javascript/controllers/play_controller.js
@@ -3,7 +3,7 @@ import { RadarController } from "chart.js";
 
 // Connects to data-controller="play"
 export default class extends Controller {
-  static targets = ["icon", "pause", "timer", "switch"]
+  static targets = ["icon", "pause", "timer", "switchstart", "switchpause"]
 
 
   connect() {
@@ -13,42 +13,42 @@ export default class extends Controller {
   toggle() {
     this.iconTarget.classList.toggle("d-none")
     this.pauseTarget.classList.toggle("d-none")
+    this.switchstartTarget.classList.toggle("d-none")
+    this.switchpauseTarget.classList.toggle("d-none")
   }
 
-  start() {
-    let temps = 0
+  start(event) {
+    // console.log(event.target.getAttribute("class"));
+    if (event.target.getAttribute("class") === "play-button") {
+      if (!this.interval) {
+        this.temps = 0
 
-    setInterval(() => {
-      let minutes = parseInt(temps / 60, 10)
-      let secondes = parseInt(temps % 60, 10)
+        this.interval = setInterval(() => {
+          let minutes = parseInt(this.temps / 60, 10)
+          let secondes = parseInt(this.temps % 60, 10)
 
-      minutes = minutes < 10 ? "0" + minutes : minutes
-      secondes = secondes < 10 ? "0" + secondes : secondes
+          minutes = minutes < 10 ? "0" + minutes : minutes
+          secondes = secondes < 10 ? "0" + secondes : secondes
 
-      this.timerTarget.innerText = `${minutes}:${secondes}`
-      temps = temps + 1
-    }, 1000)
+          this.timerTarget.innerText = `${minutes}:${secondes}`
+          this.temps = this.temps + 1
+        }, 1000)
+      }
+      else {
+        this.interval = setInterval(() => {
+          let minutes = parseInt(this.temps / 60, 10)
+          let secondes = parseInt(this.temps % 60, 10)
 
-    // let temps = 0
-    // let MyInterval = -1
+          minutes = minutes < 10 ? "0" + minutes : minutes
+          secondes = secondes < 10 ? "0" + secondes : secondes
 
-    // this.switchTarget.addEventListener("click", function(event) {
-    //   if (MyInterval = -1) {
-    //     MyInterval = setInterval(() => {
-    //       let minutes = parseInt(temps / 60, 10)
-    //       let secondes = parseInt(temps % 60, 10)
-
-    //       minutes = minutes < 10 ? "0" + minutes : minutes
-    //       secondes = secondes < 10 ? "0" + secondes : secondes
-
-    //       this.timerTarget.innerText = `${minutes}:${secondes}`
-    //       temps = temps + 1
-    //     }, 1000)
-    //   }
-    //   else {
-    //     clearInterval(MyInterval);
-    //     MyInterval = -1;
-    //   }
-    // });
+          this.timerTarget.innerText = `${minutes}:${secondes}`
+          this.temps = this.temps + 1
+        }, 1000)
+      }
+    }
+    else {
+      clearInterval(this.interval)
+    }
   }
 }
