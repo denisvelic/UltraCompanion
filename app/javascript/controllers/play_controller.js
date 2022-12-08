7eb322cd69c51e3b3a2720fc204088808414b4be
Denis Velic <denis.velic@laposte.net>
2022-12-08 12:05:34 +0100

    Add chronometer (no pause version)

--- app/javascript/controllers/play_controller.js
+++ app/javascript/controllers/play_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="play"
 export default class extends Controller {
-  static targets = ["icon", "pause"]
+  static targets = ["icon", "pause", "timer", "switch"]
 
   connect() {
     // console.log("hello");
@@ -11,5 +11,42 @@ export default class extends Controller {
   toggle() {
     this.iconTarget.classList.toggle("d-none")
     this.pauseTarget.classList.toggle("d-none")
+  }
+
+  start() {
+    let temps = 0
+
+    let Interval = setInterval(() => {
+      let minutes = parseInt(temps / 60, 10)
+      let secondes = parseInt(temps % 60, 10)
+
+      minutes = minutes < 10 ? "0" + minutes : minutes
+      secondes = secondes < 10 ? "0" + secondes : secondes
+
+      this.timerTarget.innerText = `${minutes}:${secondes}`
+      temps = temps + 1
+    }, 1000)
+
+    // let temps = 0
+    // let MyInterval = -1
+
+    // this.switchTarget.addEventListener("click", function(event) {
+    //   if (MyInterval = -1) {
+    //     MyInterval = setInterval(() => {
+    //       let minutes = parseInt(temps / 60, 10)
+    //       let secondes = parseInt(temps % 60, 10)
+
+    //       minutes = minutes < 10 ? "0" + minutes : minutes
+    //       secondes = secondes < 10 ? "0" + secondes : secondes
+
+    //       this.timerTarget.innerText = `${minutes}:${secondes}`
+    //       temps = temps + 1
+    //     }, 1000)
+    //   }
+    //   else {
+    //     clearInterval(MyInterval);
+    //     MyInterval = -1;
+    //   }
+    // });
   }
 }
