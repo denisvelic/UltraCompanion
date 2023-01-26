2f2456a93cc40f3f77c0075ca289e151df314275
Celine Brin <celine-brin@hotmail.fr>
2023-01-26 12:32:07 +0100

    jaime pas google

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
+  devise_for :user,
+  controllers: {
+     omniauth_callbacks: 'users/omniauth_callbacks'
+  }
+  # authentification google
+
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get 'weather', to: 'pages#weather'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_07_102035) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_26_091036) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,13 +40,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_07_102035) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-  end
-
-  create_table "drinking_waters", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "name"
-    t.text "gpx_file"
   end
 
   create_table "progressions", force: :cascade do |t|
@@ -89,6 +82,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_07_102035) do
     t.string "last_name"
     t.string "phone_number"
     t.text "description"
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
