0171c24381c3c9a3d140eeea64babe0eec23332e
Celine Brin <celine-brin@hotmail.fr>
2023-01-26 13:22:10 +0100

    hate google

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   # devise_for :users
-  devise_for :user,
-  controllers: {
-     omniauth_callbacks: 'users/omniauth_callbacks'
-  }
+  devise_for :user, controllers: {omniauth_callbacks: 'users/omniauth_callbacks'}
   # authentification google
 
   root to: "pages#home"
@@ -12,7 +9,6 @@ Rails.application.routes.draw do
   get 'profil', to: 'pages#profil'
   # Defines the root path route ("/")
   # root "articles#index"
-
 
   resources :races, only: %i[index show new create] do
     resource :waters, only: %i[show]
