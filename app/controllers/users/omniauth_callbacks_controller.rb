f2e070f69d3446ae383ed189a8f8006117f07abf
Celine Brin <celine-brin@hotmail.fr>
2023-02-23 18:55:53 +0100

    mails google

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -11,6 +11,10 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
         t 'devise.omniauth_callbacks.failure', kind: 'Google', reason: "#{auth.info.email} is not authorized."
       redirect_to new_user_session_path
     end
+
+    auth = request.env['omniauth.auth']
+    email = auth.info.email
+    # récupérer emails
   end
 
   protected
@@ -18,7 +22,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     def after_omniauth_failure_path_for(_scope)
       new_user_session_path
     end
-    
+
     def after_sign_in_path_for(resource_or_scope)
       stored_location_for(resource_or_scope) || root_path
     end
