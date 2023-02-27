fe347d87d126eb1a46b2db19522c80b8c8202e4b
Celine Brin <celine-brin@hotmail.fr>
2023-02-27 16:52:38 +0100

    methode strava en cours

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -13,7 +13,6 @@
 # end
 # controller pour auth avec strava
 
-
 class AuthController < ApplicationController
   def create
     auth = request.env["omniauth.auth"]
@@ -26,4 +25,22 @@ class AuthController < ApplicationController
     session[:user_id] = nil
     redirect_to root_url, notice: "Déconnecté avec succès."
   end
+
+  def strava
+    auth_hash = request.env['omniauth.auth']
+    # Récupération des informations d'authentification de Strava depuis auth_hash
+    auth_token = auth_hash['credentials']['token']
+    # Utilisation de l'API Strava pour récupérer les informations de l'utilisateur
+    strava_client = Strava::Api::V3::Client.new(access_token: auth_token)
+    athlete_info = strava_client.retrieve_current_athlete
+    # Création ou mise à jour de l'utilisateur dans la base de données de votre application
+    user = User.find_or_create_by(strava_id: athlete_info['id']) do |u|
+      u.name = athlete_info['firstname'] + ' ' + athlete_info['lastname']
+      u.access_token = auth_token
+    end
+    # Connexion de l'utilisateur dans votre application
+    session[:user_id] = user.id
+    redirect_to root_path, notice: "Vous êtes connecté avec succès avec Strava !"
+  end
+  
 end
