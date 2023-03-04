5f16e11b20cb77a1423f67d42721f4ed2aa542fb
Celine Brin <celine-brin@hotmail.fr>
2023-03-04 20:02:07 +0100

    a checker demain google

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -18,9 +18,13 @@ Devise.setup do |config|
 
   config.omniauth :google_oauth2, ENV['GOOGLE_OAUTH_CLIENT_ID'], ENV['GOOGLE_OAUTH_CLIENT_SECRET'], {
     access_type: "offline",
-    approval_prompt: ""
+    approval_prompt: "",
+    scope: 'email',
+    prompt: 'select_account',
+    redirect_uri: 'http://localhost:3000/users/auth/google_oauth2/callback'
   }
   # authentification google
+
 
   # config.omniauth :strava, ENV['STRAVA_CLIENT_ID'], ENV['STRAVA_CLIENT_SECRET']
   # authentification strava
