71df11b5115e44acc400734c49e5bad6a980a62d
pierrecaro <pierre.caro@gmail.com>
2023-03-09 18:54:13 +0100

    ajout de la géolocalisation sur la page live

--- app/javascript/controllers/live_controller.js
+++ app/javascript/controllers/live_controller.js
@@ -9,27 +9,50 @@ export default class extends Controller {
   connect() {
     mapboxgl.accessToken = this.apiKeyValue
 
-    this.map = new mapboxgl.Map({
-      container: this.element,
-      style: "mapbox://styles/mapbox/streets-v12",
-      center: [-1.553621, 47.218371],
-      zoom: 12,
-      pitch: 0
-    })
+      // const geolocate = new mapboxgl.GeolocateControl({
+      //   positionOptions: {
+      //     enableHighAccuracy: true
+      //   },
+      //   trackUserLocation: true
+      //   });
 
-    const geolocate = new mapboxgl.GeolocateControl({
-      positionOptions: {
-        enableHighAccuracy: true
+      //   // Add the control to the map.
+      //   this.map.addControl(geolocate);
+      //   // Set an event listener that fires
+      //   // when a geolocate event occurs.
+      //   geolocate.on('geolocate', () => {
+      //     console.log('A geolocate event has occurred.');
+      //   });
+
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const latitude = position.coords.latitude
+        const longitude = position.coords.longitude
+
+        console.log(latitude);
+        console.log(longitude);
+
+        this.map = new mapboxgl.Map({
+          container: this.element,
+          style: "mapbox://styles/mapbox/streets-v12",
+          center: [longitude, latitude],
+          zoom: 12,
+          pitch: 0
+        })
+
       },
-      trackUserLocation: true
-      });
+      (error) => {
+        const latitude = 47.218371
+        const longitude = -1.553621
 
-    // Add the control to the map.
-    this.map.addControl(geolocate);
-    // Set an event listener that fires
-    // when a geolocate event occurs.
-    geolocate.on('geolocate', () => {
-      console.log('A geolocate event has occurred.');
-    });
+        this.map = new mapboxgl.Map({
+          container: this.element,
+          style: "mapbox://styles/mapbox/streets-v12",
+          center: [longitude, latitude],
+          zoom: 12,
+          pitch: 0
+        })
+      }
+    )
   }
 }
