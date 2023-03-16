a80d59b3a606d2c6ec8cbd5c1b5a1fa38678568a
pierrecaro <pierre.caro@gmail.com>
2023-03-16 15:56:08 +0100

    Merge POI Stimulus controller into live stimulus controller - it's working

--- app/javascript/controllers/live_controller.js
+++ app/javascript/controllers/live_controller.js
@@ -3,37 +3,94 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="live"
 export default class extends Controller {
   static values = {
-    apiKey: String
+    geoapifyApiKey: String,
+    mapboxApiKey: String
   }
 
-  connect() {
-    // Mapbox api key.
-    mapboxgl.accessToken = this.apiKeyValue
+  async connect() {
+    // geoapify & mapbox api keys
+    const geoapifyApiKey = this.geoapifyApiKeyValue;
+    mapboxgl.accessToken = this.mapboxApiKeyValue;
+
     // Get the user's current location using the browser's geolocation API.
     navigator.geolocation.getCurrentPosition(
-       // Success callback function if user grants location access.
-      (position) => {
-        // Extract latitude and longitude from the position object.
-        const latitude = position.coords.latitude
-        const longitude = position.coords.longitude
+      // Success callback function if user grants location access.
+      async (success) => {
+      // 1. Extract latitude and longitude from the success object.
+        const longitude = success.coords.longitude
+        const latitude = success.coords.latitude
 
-        console.log(latitude);
-        console.log(longitude);
+        // 2. Query Geoapify API from URL
+        const categories = "amenity.drinking_water";
+        const filter = `circle:${longitude},${latitude},100000`;
+        const bias = `proximity:${longitude},${latitude}`;
+        const limit = 20;
+        const url = `https://api.geoapify.com/v2/places?categories=${categories}&filter=${filter}&bias=${bias}&limit=${limit}&apiKey=${geoapifyApiKey}`;
 
-        // Create a new Mapbox map with the user's location as the center.
+        // 3. Get POI from URL
+        const response = await fetch(url);
+        const data = await response.json();
+
+        // 4. Get each POI Coordinates
+        const poiCoordinates = data.features.map((feature) => {
+          return feature.geometry.coordinates;
+        });
+
+        // 5. Create a new Mapbox map with the user's location as the center.
         this.map = new mapboxgl.Map({
           container: this.element,
           style: "mapbox://styles/mapbox/streets-v12",
           center: [longitude, latitude],
           zoom: 12,
           pitch: 0
-        })
+        });
+
+        // 6. Add markers for each POI coordinate
+        poiCoordinates.forEach((coordinate) => {
+          new mapboxgl.Marker()
+            .setLngLat(coordinate)
+            .addTo(this.map);
+        });
+
+        // 7. Add the geolocate control to the map, which allows the user to center the map on their location.
+        const geolocate = new mapboxgl.GeolocateControl({
+          positionOptions: {
+            enableHighAccuracy: true
+          },
+          trackUserLocation: true
+          });
+
+          this.map.addControl(geolocate);
+
+          // Set an event listener that fires when the geolocate button is clicked.
+          geolocate.on('geolocate', () => {
+            console.log('A geolocate event has occurred.');
+        });
       },
-       // Error callback function if user denies location access.
-      (error) => {
-         // Set a default location (Nantes, France) if user denies location access
+
+      // Error callback function if user denies location access.
+      async (error) => {
+        //1. Set a default location (Nantes, France) if user denies location access
         const latitude = 47.218371
         const longitude = -1.553621
+
+        // 2. Query Geoapify API from URL
+        const categories = "amenity.drinking_water";
+        const filter = `circle:${longitude},${latitude},100000`;
+        const bias = `proximity:${longitude},${latitude}`;
+        const limit = 20;
+        const url = `https://api.geoapify.com/v2/places?categories=${categories}&filter=${filter}&bias=${bias}&limit=${limit}&apiKey=${geoapifyApiKey}`;
+
+        // 3. Get POI from URL
+        const response = await fetch(url);
+        const data = await response.json();
+
+        // 4. Get each POI Coordinates
+        const poiCoordinates = data.features.map((feature) => {
+          return feature.geometry.coordinates;
+        });
+
+        // console.log(poiCoordinates);
 
         // Create a new Mapbox map with the default location as the center.
         this.map = new mapboxgl.Map({
@@ -42,7 +99,14 @@ export default class extends Controller {
           center: [longitude, latitude],
           zoom: 12,
           pitch: 0
-        })
+        });
+
+        // 6. Add markers for each POI coordinate
+        poiCoordinates.forEach((coordinate) => {
+          new mapboxgl.Marker()
+            .setLngLat(coordinate)
+            .addTo(this.map);
+        });
       }
     )
   }
