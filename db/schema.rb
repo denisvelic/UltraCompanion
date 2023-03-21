e8f018a1dc634cbbbdc88dda8081d883f5509de6
Denis Velic <denis.velic@laposte.net>
2023-03-21 13:37:41 +0100

    migration feedback

--- db/schema.rb
+++ db/schema.rb
@@ -42,13 +42,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_21_111116) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "drinking_waters", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "name"
-    t.text "gpx_file"
-  end
-
   create_table "feedbacks", force: :cascade do |t|
     t.string "name"
     t.string "email"
