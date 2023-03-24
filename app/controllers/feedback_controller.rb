81b6a24cddba080182c5bef564d08c394557cef5
pierrecaro <pierre.caro@gmail.com>
2023-03-24 14:08:43 +0100

    Mise à jour renvoi vers email

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -1,19 +1,20 @@
 class FeedbackController < ApplicationController
   def create
+    # Create a new instance of Feedback with the parameters passed from the view
     @feedback = Feedback.new(feedback_params)
-
     if @feedback.save
-      # Send email here
-      FeedbackMailer.with(feedback: @feedback).feedback_email.deliver_now
-
+      # Send an email with the feedback details
+      # FeedbackMailer.with(feedback: @feedback).feedback_email.deliver_now
+      # Redirect to the live_path with a success message // Succes message doesn't appear
       redirect_to live_path, alert: "Votre commentaire a bien été envoyé. Merci pour votre aide !"
     else
+      # Render the new feedback form with errors
       render :new
     end
   end
 
   private
-
+  # Define a private method to allow only whitelisted parameters to be passed to the create method
   def feedback_params
     params.require(:feedback).permit(:name, :email, :suggestion)
   end
